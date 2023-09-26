7d24856547f06c62572240d68dc67467c8561233
Alan Nato <alanwnato@gmail.com>
2023-09-26 21:45:28 +0300

    fix : Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
-export default function getResponseFromAPI() {
-  const success = true;
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
+function getResponseFromAPI() {
+  return Promise.resolve();
 }
+
+export default getResponseFromAPI;
